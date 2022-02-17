f8e544400cc65551d0c59d785e574b0c102369ba
Luis Carlos - [ Frontend ] <luisestrela.bol951@gmail.com>
2022-02-16 21:03:06 -0300

    chore: configure eslint and prettier to formating code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-
 function App() {
-  return (
-    <h1>Kaylinda fernandes</h1>
-  )
+  return <h1>Kaylinda fernandes</h1>;
 }
 
-export default App
+export default App;
