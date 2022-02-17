a3c8295332462546cfb07cc3238e04b7b4a46ffd
Luis Carlos - [ Frontend ] <luisestrela.bol951@gmail.com>
2022-02-16 21:33:35 -0300

    style: add global theme and global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import theme from 'styles/theme';
+import GlobalStyles from 'styles/Global';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
